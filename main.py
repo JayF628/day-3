e951e95d5621e032bf9b8ed9db0c63d5f99294f7
Jay F <jason.finke@gmail.com>
2022-10-10 02:50:56 +0000

    new file

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+combined_string = name1 + name2
+lowercase_string = combined_string.lower()
+
+t = lowercase_string.count("t")
+r = lowercase_string.count("r")
+u = lowercase_string.count("u")
+e = lowercase_string.count("e")
+
+true = t + r + u + e
+
+l = lowercase_string.count("l")
+o = lowercase_string.count("o")
+v = lowercase_string.count("v")
+e = lowercase_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+if (love_score < 10) or (love_score > 90):
+  print(f"you're destined to fail, the score is {love_score}")
+elif love_score >=40 and love_score <=50:
+  print(f"it's possible you bozos will make it, the score is {love_score}")
+else:
+    print(f"your score is {love_score}")
+
+
+
+
+
+
